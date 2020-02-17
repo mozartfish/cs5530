972604406f33360bc978d58b99d611b79790c50d
Pranav Rajan <rajan.pranav320@gmail.com>
2020-02-17 15:58:59 -0700

    fixed all the problems with the second ER Diagram SQL Commands)

--- ER_DIAGRAMS_2.sql
+++ ER_DIAGRAMS_2.sql
@@ -1,3 +1,4 @@
+/*OLD INCORRECT SOLUTION*/
 CREATE TABLE Players(
     player_name varchar(255) NOT NULL,
     elo_rank unsigned int NOT NULL,
@@ -23,4 +24,33 @@ CREATE TABLE Games(
     event_date varchar(255) NOT NULL,
     FOREIGN KEY (event_name, event_date) REFERENCES Events(event_name, event_date)
     PRIMARY KEY(game_round, game_player_black, game_player_white)
+);
+
+/*NEW CORRECT(HOPEFULLY CORRECT) SOLUTION*/
+CREATE TABLE Players(
+    ELO UNSIGNED INT NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Primary Key(Name)
+);
+
+CREATE TABLE Events(
+    Name VARCHAR(255) NOT NULL,
+    Site VARCHAR(255) NOT NULL,
+    Date DATE NOT NULL,
+    Primary Key(Name, Site, Date)
+);
+
+CREATE TABLE Games(
+    Round UNSIGNED INTEGER NOT NULL,
+    Result CHAR(1) NOT NULL,
+    Moves VARCHAR (1000) INTEGER NOT NULL,
+    BlackPlayer VARCHAR(255) NOT NULL,
+    WhitePlayer VARCHAR(255) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Site VARCHAR(255) NOT NULL,
+    Date DATE NOT NULL,
+    Primary Key(BlackPlayer, WhitePlayer, Name, Site, Date, Round),
+    FOREIGN KEY (BlackPlayer) REFERENCES Players(Name),
+    FOREIGN KEY (WhitePlayer) REFERENCES Players(Name),
+    FOREIGN KEY (Name, Site, Date) REFERENCES Events(Name, Site, Date)
 );
