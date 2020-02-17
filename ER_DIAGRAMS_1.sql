ea6a0879a7711e29d832c98ea2d84cdfd1ee43d2
Pranav Rajan <rajan.pranav320@gmail.com>
2020-02-17 15:42:56 -0700

    fixed all the issues that I had with the first ER Diagram SQL C

--- ER_DIAGRAMS_1.sql
+++ ER_DIAGRAMS_1.sql
@@ -1,3 +1,5 @@
+
+/*OLD INCORRECT SOLUTION*/
 CREATE TABLE Persons(
     SSN char(9) NOT NULL,
     Name varchar(255) NOT NULL,
@@ -25,4 +27,40 @@ CREATE TABLE Departments(
     department_ID unsigned int NOT NULL,
     FOREIGN KEY (room_number) REFERENCES Rooms(room_number)
     PRIMARY KEY(department_id)
+);
+
+
+/*MY NEW CORRECT SOLUTION*/
+CREATE TABLE Departments(
+    dID INT UNSIGNED NOT NULL,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY(dID)
+);
+
+CREATE TABLE Rooms(
+    RmNumber INT UNSIGNED NOT NULL,
+    Capacity INT UNSIGNED NOT NULL,
+    Primary Key (RmNumber)
+    FOREIGN KEY (dID) REFERENCES Departments(dID)
+);
+
+CREATE TABLE Meetings(
+    StartTime TIME NOT NULL,
+    RmNumber INT NOT NULL,
+    FOREIGN KEY (RmNumber) REFERENCES Rooms(RmNumber)
+    Primary Key (StartTime, RmNumber)
+);
+
+CREATE TABLE People(
+    SSN CHAR(12) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Primary Key(SSN)
+);
+
+CREATE TABLE Attends(
+    SSN CHAR(12) NOT NULL,
+    RmNumber INT NOT NULL,
+    PRIMARY KEY (SSN, StartTime, RmNumber)
+    FOREIGN KEY (StartTime, RmNumber) REFERENCES Meetings(StartTime, RmNumber),
+    FOREIGN KEY (SSN) REFERENCES People(SSN)
 );
