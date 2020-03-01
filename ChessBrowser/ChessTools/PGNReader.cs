c0a4c23180e4ab4633c7db3b7722276d821cad25
Pranav Rajan <rajan.pranav320@gmail.com>
2020-03-01 00:43:56 -0700

    started the pgn class

--- ChessBrowser/ChessTools/PGNReader.cs
+++ ChessBrowser/ChessTools/PGNReader.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace ChessTools
 {
-    class PGNReader
+    /// <summary>
+    /// Class that defines a PGN Reader Object for processing and storing chess games
+    /// </summary>
+    public class PGNReader
     {
+        /// <summary>
+        /// Function that generates chess games from a PGN File
+        /// </summary>
+        /// <param name="FilePath">The path of the PGN File</param>
+        /// <returns>List of chess games</returns>
+        public List<ChessGame> GenerateChessGames(String FilePath)
+        {
+            List<ChessGame> chessGames = new List<ChessGame>();
+            return chessGames;
+        }
     }
 }
