d27cd82f8943903ca3a0c5fa651593b09bde6105
Pranav Rajan <rajan.pranav320@gmail.com>
2020-02-29 22:53:28 -0700

    added the chess browser create commands

---
+++ ChessBrowser_RAJAN_PRANAV.sql
@@ -0,0 +1,33 @@
+ CREATE TABLE `Events` (
+  `Name` varchar(255) NOT NULL,
+  `Site` varchar(255) NOT NULL,
+  `Date` date NOT NULL,
+  `eID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`eID`),
+  UNIQUE KEY `Name` (`Name`,`Date`,`Site`)
+);
+
+CREATE TABLE `Games` (
+  `Round` varchar(10) NOT NULL,
+  `Result` char(1) NOT NULL,
+  `Moves` varchar(2000) NOT NULL,
+  `BlackPlayer` int(10) unsigned NOT NULL,
+  `WhitePlayer` int(10) unsigned NOT NULL,
+  `eID` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`eID`,`Round`,`WhitePlayer`,`BlackPlayer`),
+  KEY `WhitePlayer` (`WhitePlayer`),
+  KEY `BlackPlayer` (`BlackPlayer`),
+  KEY `eID` (`eID`),
+  CONSTRAINT `Games_ibfk_1` FOREIGN KEY (`WhitePlayer`) REFERENCES `Players` (`pID`),
+  CONSTRAINT `Games_ibfk_2` FOREIGN KEY (`BlackPlayer`) REFERENCES `Players` (`pID`),
+  CONSTRAINT `Games_ibfk_3` FOREIGN KEY (`eID`) REFERENCES `Events` (`eID`)
+);
+
+CREATE TABLE `Players` (
+  `Name` varchar(255) DEFAULT NULL,
+  `Elo` int(10) unsigned DEFAULT NULL,
+  `pID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`pID`),
+  UNIQUE KEY `Name` (`Name`)
+);
+
